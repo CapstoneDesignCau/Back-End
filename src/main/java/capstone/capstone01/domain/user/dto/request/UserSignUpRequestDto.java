c67a1b309d08740fcca5261a5489ee35df689107
sinseongseop <sss02030@naver.com>
2024-10-26 15:46:35 +0900

    chore: 닉네임 사이즈 크기 8 -> 15로 수정

--- src/main/java/capstone/capstone01/domain/user/dto/request/UserSignUpRequestDto.java
+++ src/main/java/capstone/capstone01/domain/user/dto/request/UserSignUpRequestDto.java
@@ -29,7 +29,7 @@ public class UserSignUpRequestDto {
     private String name;
 
     @NotBlank(message = "닉네임은 필수 입력 항목입니다.")
-    @Size(max = 8, message = "닉네임은 최대 8자까지 입력할 수 있습니다.")
+    @Size(max = 15, message = "닉네임은 최대 15자까지 입력할 수 있습니다.")
     @Schema(description = "유저의 닉네임", example = "puang123")
     private String nickname;
 
