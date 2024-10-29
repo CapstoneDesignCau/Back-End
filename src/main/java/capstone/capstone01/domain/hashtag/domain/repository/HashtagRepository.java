727de0698513c1dd5f8ca4123ec7abf84decdbdd
sinseongseop <sss02030@naver.com>
2024-10-29 22:27:30 +0900

    Feat: Hastag Entity 정의

--- src/main/java/capstone/capstone01/domain/hashtag/domain/repository/HashtagRepository.java
+++ src/main/java/capstone/capstone01/domain/hashtag/domain/repository/HashtagRepository.java
@@ -1,9 +1,12 @@
 package capstone.capstone01.domain.hashtag.domain.repository;
 
-import capstone.capstone01.domain.comment.domain.Comment;
 import capstone.capstone01.domain.hashtag.domain.Hashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HashtagRepository extends JpaRepository<Hashtag, Long> {
+    Hashtag findByHashtag(String hashtag);
+    List<Hashtag> findByHashtagContaining(String keyword);
 
 }
