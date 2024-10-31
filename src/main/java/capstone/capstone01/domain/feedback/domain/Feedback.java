63d4c1bacd618cc8e46825c1e92bdb78fe7ec752
sinseongseop <sss02030@naver.com>
2024-10-31 21:05:18 +0900

    Feat : FeedBack entity 정의

--- src/main/java/capstone/capstone01/domain/feedback/domain/Feedback.java
+++ src/main/java/capstone/capstone01/domain/feedback/domain/Feedback.java
@@ -1,4 +1,27 @@
 package capstone.capstone01.domain.feedback.domain;
 
-public class Feedback {
+import capstone.capstone01.domain.learningmaterial.domain.LearningMaterial;
+import capstone.capstone01.global.domain.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Builder
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Feedback extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "feedbackId")
+    private Long id;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "learningMaterialId", nullable = false)
+    private LearningMaterial learningMaterial;
+
 }
