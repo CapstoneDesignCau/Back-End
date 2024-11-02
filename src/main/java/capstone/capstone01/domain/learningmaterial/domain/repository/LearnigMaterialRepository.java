542b0a2a530d675b16f861380cb819824fa8d6d2
sinseongseop <sss02030@naver.com>
2024-11-02 23:22:18 +0900

    refactor: 불필요한 파일 삭제

--- src/main/java/capstone/capstone01/domain/learningmaterial/domain/repository/LearnigMaterialRepository.java
+++
@@ -1,9 +0,0 @@
-package capstone.capstone01.domain.learningmaterial.domain.repository;
-
-
-import capstone.capstone01.domain.learningmaterial.domain.LearningMaterial;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LearnigMaterialRepository extends JpaRepository<LearningMaterial, Long> {
-
-}
