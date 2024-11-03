a84e1e9ffbc4bb9fb7c58fe1932424bba924554c
sinseongseop <sss02030@naver.com>
2024-11-03 15:00:22 +0900

    Feat: ImageEvaluation DTO 설계

--- src/main/java/capstone/capstone01/domain/imageevaluation/dto/response/ImageEvaluationResponseDto.java
+++ src/main/java/capstone/capstone01/domain/imageevaluation/dto/response/ImageEvaluationResponseDto.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
@@ -25,5 +26,14 @@ public class ImageEvaluationResponseDto {
 
     @Schema(description = "평가 이미지 URL", example = "http://example.com/image.jpg")
     private String evaluationImageUrl;
+
+    @Schema(description = "파일명", example = "example.jpg")
+    private String fileName;
+
+    @Schema(description = "확장자", example = "jpg")
+    private String extension;
+
+    @Schema(description = "생성 날짜", example = "2024-11-02T23:10:01")
+    private LocalDateTime createdAt;
 
 }
--- src/main/java/capstone/capstone01/global/util/converter/ImageEvaluationConverter.java
+++ src/main/java/capstone/capstone01/global/util/converter/ImageEvaluationConverter.java
@@ -2,6 +2,7 @@ package capstone.capstone01.global.util.converter;
 
 import capstone.capstone01.domain.imageevaluation.domain.ImageEvaluation;
 import capstone.capstone01.domain.imageevaluation.dto.response.ImageEvaluationResponseDto;
+import capstone.capstone01.domain.imageevaluation.dto.response.ImageEvaluationSummaryDto;
 import capstone.capstone01.domain.storage.domain.FileSaveInfo;
 import capstone.capstone01.domain.user.domain.User;
 
@@ -27,6 +28,20 @@ public class ImageEvaluationConverter {
                         .map(FeedbackConverter::toFeedbackResponseDto)
                         .collect(Collectors.toList()))
                 .evaluationImageUrl(imageEvaluation.getEvaluationImage().getFileUrl())
+                .fileName(imageEvaluation.getEvaluationImage().getOriginFileName())
+                .extension(imageEvaluation.getEvaluationImage().getExtension())
+                .createdAt(imageEvaluation.getCreatedAt())
+                .build();
+    }
+
+    public static ImageEvaluationSummaryDto toImageEvaluationSummaryDto(ImageEvaluation evaluation) {
+        return ImageEvaluationSummaryDto.builder()
+                .id(evaluation.getId())
+                .fileName(evaluation.getEvaluationImage().getOriginFileName())
+                .extension(evaluation.getEvaluationImage().getExtension())
+                .imageUrl(evaluation.getEvaluationImage().getFileUrl())
+                .createdAt(evaluation.getCreatedAt())
+                .isFinish(evaluation.getIsFinish())
                 .build();
     }
 
