0439a30c8c0e839cd99eeae7724cd2a2f63d794b
sinseongseop <sss02030@naver.com>
2024-11-05 23:35:18 +0900

    FIX: 프론트 서버와의  CORS 에러 해결

---
+++ src/main/java/capstone/capstone01/global/config/WebConfig.java
@@ -0,0 +1,19 @@
+package capstone.capstone01.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+
+}
